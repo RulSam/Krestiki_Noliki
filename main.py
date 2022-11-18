e927ddc061de9cf5696d043b712239ca32b159c8
Ruslan Ergashev <rergashev@gmail.com>
2022-11-18 14:20:27 +0500

    Write input (def)+

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ board = list(range(1, 10))
 
 
 def paint_board():
-    print('------------')
+    print('-------------')
     for i in range(3):
-        print('I', board[0 + i * 3], board[1 + i * 3], board[2 + i * 3], 'I')
-    print('------------')
+        print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
+    print('-------------')
+
+def write_input(Player_token):
+    value = input('Иыберите число :'+Player_token)
+    if not (value in '123456789'):
+        print('Вне диапазона. Ввведите число от 1 до 9')
+
+
 
 paint_board()
+
