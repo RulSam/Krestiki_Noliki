7fe9f6360ecfcd063c136fba12bb60a1ed6d8c3b
Ruslan Ergashev <rergashev@gmail.com>
2022-11-18 17:40:35 +0500

    Write input (def)+corrected
    + wincheck
    
    + main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 board = list(range(1, 10))
 
+win_points = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
+
 
 def paint_board():
     print('-------------')
@@ -10,17 +12,44 @@ def paint_board():
 
 def write_input(Player_token):
     while True:
-        value = input('Выберите число :' + Player_token)
-        if not (value in '123456789'):
-            print('Вне диапазона. Ввведите число от 1 до 9')
+        value = input('Выберите число: ' + Player_token)
+        if not (value in '1,2,3,4,5,6,7,8,9'):
+            print('Вне диапазона. Ввведите число от 1 до 9.')
             continue
         value = int(value)
-        if str(board[value -1] in 'XO'):
+        if str(board[value - 1]) in 'XO':
             print('Ячейка занята')
             continue
-        board[value -1] = Player_token
+        board[value - 1] = Player_token
         break
 
 
+def win_check():
+    for ech in win_points:
+        if (board[ech[0] - 1]) == (board[ech[1] - 1]) == (board[ech[2] - 1]):
+            return board[ech[1] - 1]
+    else:
+        return False
 
-paint_board()
+
+def main():
+    counter = 0
+    while True:
+        paint_board()
+        if counter % 2 == 0:
+            write_input('X')
+        else:
+            write_input('O')
+        if counter > 3:
+            winner = win_points
+            if winner:
+                paint_board()
+                print("Вы выиграли")
+                break
+        counter += 1
+        if counter > 8:
+            paint_board()
+            print('Ничья!')
+            break
+
+main()
